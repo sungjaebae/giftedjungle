a8ce1db042e3d4a35ba6c85a5361ef77d4158e41
sungjae bae <hsm0156@gmail.com>
2023-10-11 23:03:15 +0900

    init_db.py 구현, 레이아웃 구현

--- init_db.py
+++ init_db.py
@@ -1,22 +1,35 @@
 from pymongo import MongoClient
-
+import json
 client = MongoClient('localhost', 27017)
 db = client.dbgiftedjungle
 
 
 def insert_user():
-    pass
+    with open('./static/seed_data/user.json', 'r', encoding='UTF8') as f:
+        data = json.load(f)
+        db.users.insert_many(data)
+
+
+def insert_category():
+    with open('./static/seed_data/category.json', 'r', encoding='UTF8') as f:
+        data = json.load(f)
+        db.categories.insert_many(data)
 
 
 def insert_gift():
-    pass
+    with open('./static/seed_data/gift.json', 'r', encoding='UTF8') as f:
+        data = json.load(f)
+        db.gifts.insert_many(data)
 
 
 def drop_all():
-    pass
+    db.users.delete_many({})
+    db.categories.delete_many({})
+    db.gifts.delete_many({})
 
 
 if __name__ == '__main__':
     drop_all()
     insert_user()
+    insert_category()
     insert_gift()
--- routes/gift.py
+++ routes/gift.py
@@ -1,11 +1,16 @@
+from bson import ObjectId
 from flask import Blueprint, render_template
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbgiftedjungle
 
 gift = Blueprint('gift', __name__)
 
 
 @gift.route('/gift')
 def gift_list():
-    return render_template('index.html')  # 선물 목록 페이지
+    categories = list(db.categories.find({}, {}))
+    return render_template('index.html', categories=categories)  # 선물 목록 페이지
 
 
 @gift.route('/gift/<id>')
@@ -16,3 +21,11 @@ def gift_detail(id):
 @gift.route('/recipient')
 def recipient():
     return render_template('recipient.html')  # 수령인 선택 페이지
+
+
+@gift.route('/api/category/<id>')
+def gifts_of_category(id):
+    # 이 부분이 매우 중요합니다. category.name이 아니라 category["name"]으로 해야 합니다.
+    category = db.categories.find_one({'_id': ObjectId(id)}, {})
+    gifts = db.gifts.find({'category.name': category["name"]}, {})
+    return list(gifts)
