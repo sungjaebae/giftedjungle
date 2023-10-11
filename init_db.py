52554b6be9fd61bea40a28cc7af433f7d334db0f
LSY <kongma0312@gmail.com>
2023-10-12 00:09:07 +0900

    first/10.12

--- init_db.py
+++ init_db.py
@@ -1,15 +1,45 @@
-from pymongo import MongoClient
+import random
+import requests
+
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  
 
 client = MongoClient('localhost', 27017)
 db = client.dbgiftedjungle
-
+#db.user.deleteMany({})
+#db.notification.deleteMany({})
 
 def insert_user():
-    pass
+    userdoc1={
+        'usrid' : 1,
+        'usrimg' : "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg",
+        'usrname' : "김훈이",
+        'usrpassword': 1234,
+    }
+
+    userdoc2={
+        'usrid' : 2,
+        'usrimg' : "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg",
+        'usrname' : "김철수",
+        'usrpassword': 1234,
+    }
+    db.user.insert_one(userdoc1)
+    db.user.insert_one(userdoc2)
+    print('유저 정보 입력 완료')
 
 
 def insert_gift():
-    pass
+    doc ={
+        'giftimg' : "https://previews.123rf.com/images/ksuperksu/ksuperksu1601/ksuperksu160100007/51544658-%EB%82%B4-%EC%A0%90%EC%8B%AC-%EC%8B%9D%EC%82%AC-%EB%A7%9B%EC%9E%88%EB%8A%94-%EC%8B%A0%EC%84%A0%ED%95%9C-%EC%9D%8C%EC%8B%9D%EC%9D%98-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-%EC%8F%B4-%EC%8B%9D%EC%82%AC%EC%9D%98-%EC%A1%B0%EC%84%B1%EC%9E%85%EB%8B%88%EB%8B%A4-%EB%B3%91-%EC%9D%8C%EB%A3%8C-%EC%83%8C%EB%93%9C%EC%9C%84%EC%B9%98-%EC%83%90%EB%9F%AC%EB%93%9C-%EC%82%AC%EA%B3%BC-%EC%B4%88%EC%BD%9C%EB%A6%BF-%EC%83%8C%EB%93%9C%EC%9C%84%EC%B9%98-%EC%BF%A0%ED%82%A4%EC%99%80-%EB%B2%84%ED%84%B0-%EC%83%9D-%EA%B3%BC%EC%9E%90%EC%9E%85%EB%8B%88%EB%8B%A4.jpg",
+        'giftname' : "10월 15일 점심",
+        'giftmsg' : "밥먹자",
+        'sentusr' : 1,
+        'receiptionusr' : 2,
+        'ischeck' : 1,
+        'isaccept' : 1,
+    }
+    db.notification.insert_one(doc)
+    print('완료 : ')
 
 
 def drop_all():
--- routes/notification.py
+++ routes/notification.py
@@ -1,11 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+from bson import ObjectId
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+from flask.json.provider import JSONProvider
+
+import json
+import sys
+
+
 
 notification = Blueprint('notification', __name__)
-
+client = MongoClient('localhost', 27017)
+db = client.dbgiftedjungle
 
 @notification.route('/notification')
 def notification_list():
-    return render_template('notification_list.html')  # 알림 목록 페이지
+
+    myid = request.args.get("myid", 0)
+    
+    notifications = list(db.notification.find({"receiptionusr" : myid}))
+
+    # let giftname = notification['giftname']
+    # let giftmsg = notification['giftmsg']
+    # let sentusr = notification['sentusr']
+    # let receiptionusr = notification['receiptionusr']
+    # let ischeck = notification['ischeck']
+    #       let isaccept = notification['isaccept']
+
+
+    # notifications = list(db.notification.find({"$or":[
+    #     {"sentusr" : myid},
+    #     {"receiptionusr" : myid}
+    # ]}))
+
+    notifications = list(db.notification.find({}))
+    return render_template('notification_list.html', notifications = notifications)    # 알림 목록 페이지
 
 
 @notification.route('/notification/<id>')
