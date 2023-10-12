6017f7306215f7e1d6bca1cf5b64001c7ae04f17
LSY <kongma0312@gmail.com>
2023-10-12 12:02:58 +0900

    second/10.12_api(Jinja.ver)

--- init_db.py
+++ init_db.py
@@ -6,9 +6,6 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.dbgiftedjungle
-#db.user.deleteMany({})
-#db.notification.deleteMany({})
-
 def insert_user():
     userdoc1={
         'usrid' : 1,
@@ -30,18 +27,38 @@ def insert_user():
 
 def insert_gift():
     doc ={
-        'giftimg' : "https://previews.123rf.com/images/ksuperksu/ksuperksu1601/ksuperksu160100007/51544658-%EB%82%B4-%EC%A0%90%EC%8B%AC-%EC%8B%9D%EC%82%AC-%EB%A7%9B%EC%9E%88%EB%8A%94-%EC%8B%A0%EC%84%A0%ED%95%9C-%EC%9D%8C%EC%8B%9D%EC%9D%98-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-%EC%8F%B4-%EC%8B%9D%EC%82%AC%EC%9D%98-%EC%A1%B0%EC%84%B1%EC%9E%85%EB%8B%88%EB%8B%A4-%EB%B3%91-%EC%9D%8C%EB%A3%8C-%EC%83%8C%EB%93%9C%EC%9C%84%EC%B9%98-%EC%83%90%EB%9F%AC%EB%93%9C-%EC%82%AC%EA%B3%BC-%EC%B4%88%EC%BD%9C%EB%A6%BF-%EC%83%8C%EB%93%9C%EC%9C%84%EC%B9%98-%EC%BF%A0%ED%82%A4%EC%99%80-%EB%B2%84%ED%84%B0-%EC%83%9D-%EA%B3%BC%EC%9E%90%EC%9E%85%EB%8B%88%EB%8B%A4.jpg",
+        'id' : 10,
+        'giftimg' : "/img/launch.jpg",
         'giftname' : "10월 15일 점심",
         'giftmsg' : "밥먹자",
-        'sentusr' : 1,
-        'receiptionusr' : 2,
-        'ischeck' : 1,
-        'isaccept' : 1,
+        'sentusrid' : 1,
+        'sentusrname' : "김훈이",
+        'sentusrimg' : "/img/usrimg.png",
+        'receivedusrid' : 2,
+        'receivedusrname' : "김영희",
+        'ischeck' : False,
+        'isaccept' : False,
     }
     db.notification.insert_one(doc)
     print('완료 : ')
 
-
+def insert_received():
+    receiveddoc={
+        'id' : 10,
+        #'giftimg' : "C:\Users\푸린\Desktop\activity\jungle\0주차\giftedjungle\templates\img\launch.jpeg",
+        'giftname' : "10월 15일 점심",
+        'giftmsg' : "밥먹자",
+        'sentusrid' : 1,
+        'sentusrname' : "김훈이",
+        #'sentusrimg' : "C:\Users\푸린\Desktop\activity\jungle\0주차\giftedjungle\templates\img\usrimg.png",
+        'receiptionusrid' : 2,
+        'receiptionusrname' : "김영희",
+        #'receiptionusrimg' : "C:\Users\푸린\Desktop\activity\jungle\0주차\giftedjungle\templates\img\usrimg.png",
+        'ischeck' : False,
+        'isaccept' : False,
+    }
+    db.received.insert_one(receiveddoc)
+    print('받은선물 입력 완료 : ')
 def drop_all():
     pass
 
@@ -50,3 +67,4 @@ if __name__ == '__main__':
     drop_all()
     insert_user()
     insert_gift()
+    insert_received()
--- routes/notification.py
+++ routes/notification.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, jsonify
 from bson import ObjectId
-from pymongo import MongoClient
+from pymongo import MongoClient,ReturnDocument
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request,redirect,url_for
 from flask.json.provider import JSONProvider
 
 import json
@@ -16,33 +16,48 @@ db = client.dbgiftedjungle
 
 @notification.route('/notification')
 def notification_list():
-
-    myid = request.args.get("myid", 0)
-    
-    notifications = list(db.notification.find({"receiptionusr" : myid}))
-
-    # let giftname = notification['giftname']
-    # let giftmsg = notification['giftmsg']
-    # let sentusr = notification['sentusr']
-    # let receiptionusr = notification['receiptionusr']
-    # let ischeck = notification['ischeck']
-    #       let isaccept = notification['isaccept']
-
-
-    # notifications = list(db.notification.find({"$or":[
-    #     {"sentusr" : myid},
-    #     {"receiptionusr" : myid}
-    # ]}))
-
     notifications = list(db.notification.find({}))
     return render_template('notification_list.html', notifications = notifications)    # 알림 목록 페이지
 
 
 @notification.route('/notification/<id>')
-def notification_detail():
-    return render_template('notification.html')  # 알림 상세 페이지
+def notification_detail(id):
+    notificationdetail = db.notification.find_one({'id':id})
+    return render_template('notification.html', notificationdetail = notificationdetail)  # 알림 상세 페이지
 
+@notification.route('/notification/accept/<id>')
+def update_accept_received(id):
+    print("id : ", id)
+    db.notification.find_one_and_update({'id':10}, {'$set':{'ischeck':True}})
+    db.notification.find_one_and_update({'id':10}, {'$set':{'isaccept':True}}, return_document=ReturnDocument.AFTER )
+    returnnotification= db.notification.find_one({'id':10},{'_id':0})
+    # if(returnnotification==None):
+    #     print("받은 선물함 업데이트 실패!")
+    #     return
+    # else:
+    print(returnnotification)
+    db.received.insert_one(returnnotification)
+    return redirect(url_for('notification.received_gift'))
+    #return render_template('received_gift.html')
+
+@notification.route('/notification/refuse/<id>')
+def update_refuse_received(id):
+    print("id : ", id)
+    db.notification.find_one_and_update({'id':10}, {'$set':{'ischeck':True}})
+    db.notification.find_one_and_update({'id':10}, {'$set':{'isaccept':False}}, return_document=ReturnDocument.AFTER )
+    returnnotification= db.notification.find_one({'id':10},{'_id':0})
+    # if(returnnotification==None):
+    #     print("받은 선물함 업데이트 실패!")
+    #     return
+    # else:
+    print(returnnotification)
+    db.received.insert_one(returnnotification)
+    
+    notifications = list(db.notification.find({})) 
+    #return redirect(url_for('notification.notification_list'))
+    return render_template('notification_list.html', notifications = notifications)
 
 @notification.route('/received_gift')
 def received_gift():
-    return render_template('received_gift.html')  # 받은 선물함 페이지
+    received = list(db.received.find({}))
+    return render_template('received_gift.html', received=received)  # 받은 선물함 페이지
