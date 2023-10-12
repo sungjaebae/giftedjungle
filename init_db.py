cbdd1d75232f7c36c1626faa7c20e52b0b29b899
sungjae bae <hsm0156@gmail.com>
2023-10-13 01:03:35 +0900

    로그인, 선물, 알림 통합

--- init_db.py
+++ init_db.py
@@ -1,57 +1,34 @@
 from pymongo import MongoClient
 import json
-client = MongoClient('localhost', 27017)
+client: MongoClient = MongoClient('localhost', 27017)
 db = client.dbgiftedjungle
-#db.user.deleteMany({})
-#db.notification.deleteMany({})
+# db.user.deleteMany({})
+# db.notification.deleteMany({})
+
 
 def insert_user():
-    userdoc1={
-        'usrid' : 1,
-        #'usrimg' : "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg",
-        'usrname' : "김훈이",
-        'usrpassword': 1234,
-    }
-
-    userdoc2={
-        'usrid' : 2,
-        #'usrimg' : "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg",
-        'usrname' : "김철수",
-        'usrpassword': 1234,
-    }
-    db.user.insert_one(userdoc1)
-    db.user.insert_one(userdoc2)
-    print('유저 정보 입력 완료')
-
     with open('./static/seed_data/user.json', 'r', encoding='UTF8') as f:
         data = json.load(f)
         db.users.insert_many(data)
+
 
 def insert_notification():
-    doc ={
-        'notif_id' : 10,
-        'url' : "../static/images/food.webp",
-        #'gift-img' : "/img/launch.jpg",
-        'giftname' : "10월 15일 점심",
-        'giftmsg' : "밥먹자",
-        'sndusrid' : 1,
-        'sndusrname' : "김훈이",
-        #'snd-usr-img' : "/img/usrimg.png",
-        'rcvusrid' : 2,
-        'rcvusrname' : "김영희",
-        #'rcv-usr-img' : "/img/usrimg.png",
-        'ischeck' : False,
-        'isaccept' : False,
+    doc = {
+        'notif_id': 10,
+        'url': "../static/images/food.webp",
+        # 'gift-img' : "/img/launch.jpg",
+        'giftname': "10월 15일 점심",
+        'giftmsg': "밥먹자",
+        'sndusrid': 1,
+        'sndusrname': "김훈이",
+        # 'snd-usr-img' : "/img/usrimg.png",
+        'rcvusrid': 2,
+        'rcvusrname': "김영희",
+        # 'rcv-usr-img' : "/img/usrimg.png",
+        'ischeck': False,
+        'isaccept': False,
     }
     db.notification.insert_one(doc)
-    print('완료 : ') 
-
-
-def insert_git():
-    print('완료 : ')
-    with open('./static/seed_data/user.json', 'r', encoding='UTF8') as f:
-        data = json.load(f)
-        db.users.insert_many(data)
 
 
 def insert_category():
