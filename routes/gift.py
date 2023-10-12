a78c47ac63fe36425885c9559520d57f6dd6944f
sungjae bae <hsm0156@gmail.com>
2023-10-12 16:03:27 +0900

    알림 생성 이전 구현 완료

--- routes/gift.py
+++ routes/gift.py
@@ -1,5 +1,5 @@
 from bson import ObjectId
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbgiftedjungle
@@ -15,17 +15,32 @@ def gift_list():
 
 @gift.route('/gift/<id>')
 def gift_detail(id):
-    return render_template('gift.html')  # 선물 상세 페이지
+    gift = db.gifts.find_one({'_id': ObjectId(id)}, {})
+    return render_template('gift.html', gift=gift)  # 선물 상세 페이지
 
 
-@gift.route('/recipient')
+@gift.route('/recipient', methods=['POST'])
 def recipient():
-    return render_template('recipient.html')  # 수령인 선택 페이지
+    message = request.form["message"]
+    gift_id = request.form["gift_id"]
+    users = list(db.users.find({}, {}))
+    # 수령인 선택 페이지
+    return render_template('recipient.html', message=message, gift_id=gift_id, users=users)
+
+
+@gift.route('/process_gift', methods=['POST'])
+def process_gift():
+    message = request.form["message"]
+    gift_id = request.form["gift_id"]
+    recipient_id = request.form["recipient_id"]
+    return redirect('/')
+    # return redirect(url_for('index')) 이렇게 하는걸 추천한다고 한다
 
 
 @gift.route('/api/category/<id>')
 def gifts_of_category(id):
-    # 이 부분이 매우 중요합니다. category.name이 아니라 category["name"]으로 해야 합니다.
+    # category.name이 아니라 category["name"]으로 해야 한다
     category = db.categories.find_one({'_id': ObjectId(id)}, {})
     gifts = db.gifts.find({'category.name': category["name"]}, {})
-    return list(gifts)
+    gifts = list(gifts)
+    return gifts
