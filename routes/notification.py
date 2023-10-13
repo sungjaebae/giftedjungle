ff3f64068e0381a3db5bafac26cad7b7ac20dde3
sungjae bae <hsm0156@gmail.com>
2023-10-13 12:53:24 +0900

    받은 선물함 수정

--- routes/notification.py
+++ routes/notification.py
@@ -13,14 +13,18 @@ SECRET_KEY = 'JUNGLE'
 @notification.route('/notification')
 def notification_list():
     token_receive = request.cookies.get('mytoken')
-    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    user = db.users.find_one({'id': payload['id']}, {})
-    notifications = list(db.notifications.find(
-        {"$or": [{"sender.id": user["id"]}, {"recipient.id": user["id"]}]}))
-    print('user->', user)
-    print(notifications)
-    # 알림 목록 페이지
-    return render_template('notification_list.html', notifications=notifications, myid=user['id'])
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user = db.users.find_one({'id': payload['id']}, {})
+        notifications = list(db.notifications.find(
+            {"$or": [{"sender.id": user["id"], "is_deleted": False}, {"recipient.id": user["id"], "is_deleted": False}]}))
+        # 알림 목록 페이지
+        return render_template('notification_list.html', notifications=notifications, myid=user['id'])
+    except jwt.ExpiredSignatureError:
+        return redirect("/login")
+    except jwt.exceptions.DecodeError:
+        return redirect("/login")
 
 
 @notification.route('/notification/<notif_id>')
@@ -31,42 +35,40 @@ def notification_detail(notif_id):
     return render_template('notification.html', notificationdetail=notificationdetail)
 
 
-@notification.route('/notification/accept/<notif_id>')
-def update_accept_received(notif_id):
-    db.notifications.find_one_and_update(
-        {'_id': notif_id}, {'$set': {'is_read': True}})
-    db.notifications.find_one_and_update({'_id': notif_id}, {
-                                        '$set': {'is_deleted': False}}, return_document=ReturnDocument.AFTER)
-    returnnotification = db.notifications.find_one(
-        {'_id': notif_id}, {'_id': 0})
-    print(returnnotification)
-    db.received.insert_one(returnnotification)
-    return redirect(url_for('notification.received_gift'))
-    # return render_template('received_gift.html')
+@notification.route('/process_accept', methods=['POST'])
+def update_accept_received():
+    notif_id = request.form["notif_id"]
+    notification = db.notifications.find_one_and_update({'_id': ObjectId(notif_id)}, {
+        '$set': {'is_read': True, 'is_deleted': True}})
+
+    db.received_gifts.insert_one(
+        {'gift': notification['gift'], 'notification': notification})
+    return redirect('/received_gift')
 
 
-@notification.route('/notification/refuse/<notif_id>')
-def update_refuse_received(notif_id):
-    db.notifications.find_one_and_update(
-        {'_id': notif_id}, {'$set': {'is_read': True}})
-    db.notifications.find_one_and_update({'_id': notif_id}, {
-                                        '$set': {'is_deleted': False}}, return_document=ReturnDocument.AFTER)
-    returnnotification = db.notifications.find_one(
-        {'_id': notif_id}, {'_id': 0})
-    print(returnnotification)
-    # db.received.insert_one(returnnotification)
+@notification.route('/process_deny', methods=['POST'])
+def update_refuse_received():
+    notif_id = request.form["notif_id"]
+    db.notifications.update_one({'_id': ObjectId(notif_id)}, {
+        '$set': {'is_read': True, 'is_deleted': True}})
 
-    notifications = list(db.notifications.find({}))
-    # return redirect(url_for('notification.notification_list'))
-    return render_template('notification_list.html', notifications=notifications)
+    return redirect('/notification')
 
 
 @notification.route('/received_gift')
 def received_gift():
-    notifications = list(db.notifications.find(
-        {"$or": [{"sender.id": user["id"]}, {"recipient.id": user["id"]}]}))
-    
-    received = list(db.notifications.find(
-        {'$and': [{'is_read': True}, {'is_deleted': False}]}))
-    # 받은 선물함 페이지
-    return render_template('received_gift.html', received=received)
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        # token디코딩합니다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user = db.users.find_one({'id': payload['id']}, {})
+        received_gifts = list(db.received_gifts.find(
+            {'notification.recipient.id': user['id']}))
+        print(received_gifts[0])
+        # 받은 선물함 페이지
+        return render_template('received_gift.html', received_gifts=received_gifts)
+    except jwt.ExpiredSignatureError:
+        return redirect("/login")
+    except jwt.exceptions.DecodeError:
+        return redirect("/login")
